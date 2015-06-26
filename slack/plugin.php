dcd3bb7a3409cc0163ee5d9078052d6b1f07ead7
EvertEt <evert.etienne@telenet.be>
2015-06-26 15:09:15 +0200

    preparations for PR

--- slack/plugin.php
+++ slack/plugin.php
@@ -10,11 +10,11 @@ set_include_path(get_include_path().PATH_SEPARATOR.dirname(__file__).'/include')
 
 return array(
     'id' =>           'osticket:slack', 
-    'version' =>      '1.0',
+    'version' =>      '0.2',
     'name' =>         'Slack notifier',
-    'author' =>       'Quivr',
+    'author' =>       'Thammanna Jammada',
     'description' =>  'Notify Slack on new ticket.',
-    'url' =>          'https://github.com/Quivr/osticket-slack',
+    'url' =>          'https://github.com/thammanna/osticket-slack',
     'plugin' =>       'slack.php:SlackPlugin',  //file name:class name
 );
 
